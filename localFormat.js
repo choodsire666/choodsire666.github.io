03df81c0fbc06e9ac9cb0ee0029bf5ca7384d5ed
chood <a1609981289@gmail.com>
2024-05-28 22:18:22 +0800

    发布

--- localFormat.js
+++ localFormat.js
@@ -1,5 +1,5 @@
 // 0.12.0及以上版本用法
-const { matterMarkdownAdapter } = require('@elog/cli')
+const { matterMarkdownAdapter, request } = require('@elog/cli')
 
 /**
  * 自定义文档插件
@@ -12,7 +12,17 @@ const format = async (doc, imageClient) => {
   // 将 cover 字段中的 notion 图片下载到本地
   if (imageClient)  {
     // 只有启用图床平台image.enable=true时，imageClient才能用，否则请自行实现图片上传
-    const url = await imageClient.uploadImageFromUrl(cover, doc)
+    const url = ''
+    if (cover) {
+      url = await imageClient.uploadImageFromUrl(cover, doc)
+    } else {
+      let res = await request('https://www.dmoe.cc/random.php?return=json')
+      if (res.code === 200) {
+        url = res.imgurl
+      } else {
+        url = ''
+      }
+    }
     // cover链接替换为本地图片
     doc.properties.cover = url
   }
