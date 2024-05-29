c51a7e660b51d97e7051a91b8d55e05f98299423
chood <a1609981289@gmail.com>
2024-05-29 19:27:18 +0800

    发布

--- elog.format.js
+++ elog.format.js
@@ -1,5 +1,6 @@
 // 0.12.0及以上版本用法
-const { matterMarkdownAdapter, out, request } = require('@elog/cli')
+const { matterMarkdownAdapter, out, request, getPicBufferFromURL} = require('@elog/cli')
+const { GithubClient } = require('./github')
 
 /**
  * 自定义文档插件
@@ -8,32 +9,39 @@ const { matterMarkdownAdapter, out, request } = require('@elog/cli')
  * @return {Promise<DocDetail>} 返回处理后的文档对象
  */
 const format = async (doc, imageClient) => {
-  out.info("封面图片替换")
-  const cover = doc.properties.cover
-  // 将 cover 字段中的 notion 图片下载到本地
-  if (cover && imageClient)  {
+  let title = doc.properties.title
+  out.info(title + "封面图片替换")
+  let cover = doc.properties.cover
+
+  // cover不存在
+  if (!cover) {
+    out.info(title + "不存在封面图片！")    
+    let res = await request('https://www.dmoe.cc/random.php?return=json')
+    if (res.data.code === '200') {
+      const buffer = await getPicBufferFromURL(res.data.imgurl)      
+      let newUrl = await imageClient.ctx.uploadImg(buffer, 'cover.jpg', doc)
+      if (newUrl) {
+        out.info('上传成功', newUrl)
+        doc.properties.cover = newUrl    
+      } else {
+        out.warning('上传失败：' + 'cover.jpg' + ' 请检查图床配置')
+      }
+    } else {
+      out.warning('获取失败：' + 'https://www.dmoe.cc/random.php?return=json' + ' 请检查网络连接')
+    } 
+  } else if (imageClient) {
     // 只有启用图床平台image.enable=true时，imageClient才能用，否则请自行实现图片上传
     const url = await imageClient.uploadImageFromUrl(cover, doc)
     // cover链接替换为本地图片
     doc.properties.cover = url
-  }
+  }  
 
   if (doc.properties.cover) {
-    out.info("封面图片替换成功")
+    out.info(title + "封面图片替换成功")
   } else {
-    let res = await request('https://www.dmoe.cc/random.php?return=json')   
-    res = res.data 
-    for (let prop in res) {
-      if (res.hasOwnProperty(prop)) {
-        out.info(`${prop}: ${res[prop]}`)
-      }
-    }
-    if (res.code === 200) {
-      doc.properties.cover = res.imgurl
-    } else {
-      out.info("封面图片替换失败")
-    }
+    out.info(title + "封面图片替换失败")
   }
+
   doc.body = matterMarkdownAdapter(doc);
   return doc;
 };
--- source/_posts/yuque/smartlock.md
+++
@@ -1,9 +0,0 @@
----
-title: smartlock
-urlname: mk2dsz9o053qcbtb
-date: '2024-05-15 13:33:57'
-updated: '2024-05-15 13:34:50'
-cover: 'https://raw.githubusercontent.com/choodsire666/blog-img/main/smartlock/de16092dac6abb41710211d14a5372a9.png'
----
-![image.png](https://raw.githubusercontent.com/choodsire666/blog-img/main/smartlock/de16092dac6abb41710211d14a5372a9.png)
-
